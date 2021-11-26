9a34ec61f5e61168f42022918e8c5a874022da01
syth0le <chdan565@gmail.com>
2021-11-27 00:18:53 +0300

    changes in pdf

--- app/pdf.py
+++ app/pdf.py
@@ -3,8 +3,6 @@ import glob
 from PIL import Image
 from fpdf import FPDF
 
-pdf = FPDF()
-
 im1 = Image.open("/coding/pdf-bot/static/images/1.jpg")
 im2 = Image.open("/coding/pdf-bot/static/images/2.jpg")
 im3 = Image.open("/coding/pdf-bot/static/images/3.jpg")
@@ -13,10 +11,14 @@ imagelist = [im2, im3]
 pdf1_filename = "/coding/pdf-bot/static/pdf/bbd1.pdf"
 
 im1.save(pdf1_filename, "PDF", resolution=100.0, save_all=True, append_images=imagelist)
+########################################################################################
+pdf = FPDF()
+pdf.set_auto_page_break(0)
 
-imagelist = glob.glob('static/images/*.jpg')
+imagelist = glob.glob('/coding/pdf-bot/static/images/*.jpg')
 
 for image in imagelist:
     pdf.add_page()
-    pdf.image(image, 10, 210, 297)
+    pdf.image(image, w=190, h=280)
+    print(pdf)
 pdf.output("/coding/pdf-bot/static/pdf/yourfile.pdf", "F")
