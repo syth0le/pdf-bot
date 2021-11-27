356f91206f24080fa3112e1baeb2e2d9263e6907
syth0le <chdan565@gmail.com>
2021-11-27 18:14:13 +0300

    delete useless part of code

--- app/pdf.py
+++ app/pdf.py
@@ -1,7 +1,6 @@
 import glob
 import os
 
-from PIL import Image
 from fpdf import FPDF
 
 
@@ -19,14 +18,3 @@ def pdf_main() -> str:
         os.remove(image)
     pdf.output("/coding/pdf-bot/static/pdf/yourfile.pdf", "F")
     return "/coding/pdf-bot/static/pdf/yourfile.pdf"
-
-
-def pdf_sub():
-    im1 = Image.open("/coding/pdf-bot/static/images/1.jpg")
-    im2 = Image.open("/coding/pdf-bot/static/images/2.jpg")
-    im3 = Image.open("/coding/pdf-bot/static/images/3.jpg")
-    imagelist = [im2, im3]
-
-    pdf1_filename = "/coding/pdf-bot/static/pdf/bbd1.pdf"
-
-    im1.save(pdf1_filename, "PDF", resolution=100.0, save_all=True, append_images=imagelist)
