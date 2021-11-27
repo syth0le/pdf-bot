b2f0c85cf39fa51a44b60abcf3377c0d91bfbde5
syth0le <chdan565@gmail.com>
2021-11-27 18:10:44 +0300

    update pdf convert function

--- app/pdf.py
+++ app/pdf.py
@@ -5,11 +5,13 @@ from PIL import Image
 from fpdf import FPDF
 
 
-def pdf_main():
+def pdf_main() -> str:
     pdf = FPDF()
     pdf.set_auto_page_break(0)
 
     imagelist = glob.glob('/coding/pdf-bot/static/images/*.jpg')
+    if not imagelist:
+        return "There're no pictures to convert"
 
     for image in imagelist:
         pdf.add_page()
--- app/server.py
+++ app/server.py
@@ -2,14 +2,14 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from app.config import BotConfig
+from app.middleware import AccessMiddleware
 from app.pdf import pdf_main
 
 bot = Bot(token=BotConfig.API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-
-# dp.middleware.setup(middleware=AccessMiddleware(BotConfig.ACCESS_ID))
+dp.middleware.setup(middleware=AccessMiddleware(BotConfig.ACCESS_ID))
 
 
 @dp.message_handler(commands=['start', 'help', 'menu'])
@@ -32,4 +32,7 @@ async def handle_docs_photo(message):
 @dp.message_handler(commands=['test'])
 async def send_pdf(message: types.Message):
     name = pdf_main()
-    await bot.send_document(message.chat.id, open(name, 'rb'))
+    try:
+        await bot.send_document(message.chat.id, open(name, 'rb'))
+    except FileNotFoundError:
+        await bot.send_message(message.chat.id, name)
