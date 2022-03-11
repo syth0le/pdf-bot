f833af47fcb408cbddd788e27220457df2fc9a8c
syth0le <chdan565@gmail.com>
2022-03-11 15:19:41 +0300

    add handle_docs_photo 8

--- app/pdf.py
+++ app/pdf.py
@@ -6,7 +6,9 @@ from PIL import Image
 
 
 def get_pdf(chat_id: int) -> str:
-    images = glob.glob(f'/coding/pdf-bot/static/images/{chat_id}/*.jpg')
+    destination_file = os.getcwd() + f"/static/images/{chat_id}/{chat_id}-{time_for_image}-{photo.file_unique_id}.jpg"
+
+    images = glob.glob(os.getcwd() + f'/static/images/{chat_id}/*.jpg')
     if not images:
         return "There're no pictures to convert"
 
@@ -17,7 +19,7 @@ def get_pdf(chat_id: int) -> str:
         os.remove(image)
 
     time_for_pdf = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    pdf_path = f'/coding/pdf-bot/static/pdf/{chat_id}-{time_for_pdf}.pdf'
+    pdf_path = os.getcwd() + f'/static/pdf/{chat_id}-{time_for_pdf}.pdf'
     image_list[0].save(pdf_path, save_all=True,
                        append_images=image_list[1:])
     return pdf_path
