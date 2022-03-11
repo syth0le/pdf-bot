80062faa373cf1ff19247c67d95260f963a15377
syth0le <chdan565@gmail.com>
2022-03-11 15:20:55 +0300

    add handle_docs_photo 9

--- app/pdf.py
+++ app/pdf.py
@@ -6,8 +6,6 @@ from PIL import Image
 
 
 def get_pdf(chat_id: int) -> str:
-    destination_file = os.getcwd() + f"/static/images/{chat_id}/{chat_id}-{time_for_image}-{photo.file_unique_id}.jpg"
-
     images = glob.glob(os.getcwd() + f'/static/images/{chat_id}/*.jpg')
     if not images:
         return "There're no pictures to convert"
