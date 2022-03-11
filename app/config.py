977d5b3ed4c4f132faba464f04cdaf01264cd103
syth0le <chdan565@gmail.com>
2022-03-11 16:21:25 +0300

    added chat support

--- app/config.py
+++ app/config.py
@@ -4,8 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
+IS_POSTING_REQUESTED = True
+
 
 class BotConfig:
-    API_TOKEN = os.getenv("API_TOKEN")
-    ACCESS_ID = os.getenv("ACCESS_ID")
-    USERNAME = os.getenv("USERNAME")
+    API_TOKEN = os.getenv('API_TOKEN')
+    ACCESS_ID = os.getenv('ACCESS_ID')
+    USERNAME = os.getenv('USERNAME')
+    CHAT_ID = os.getenv('CHAT_ID')
--- app/server.py
+++ app/server.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from app import constants
+from app import constants, config
 from app.config import BotConfig
 from app.middleware import AccessMiddleware
 from app.pdf import get_pdf
@@ -43,9 +43,18 @@ async def handle_docs_photo(message):
 
 
 @dp.message_handler(commands=['conv2pdf'])
+async def get_text_of_post(message: types.Message):
+    config.IS_POSTING_REQUESTED = True
+    await bot.send_message(message.chat.id, 'Please, send text of new post:')
+
+
+@dp.message_handler()
 async def send_pdf(message: types.Message):
     name = get_pdf(chat_id=message.chat.id)
-    try:
-        await bot.send_document(message.chat.id, open(name, 'rb'))
-    except FileNotFoundError:
-        await bot.send_message(message.chat.id, name)
+    if config.IS_POSTING_REQUESTED:
+        config.IS_POSTING_REQUESTED = False
+        try:
+            await bot.send_document(message.chat.id, open(name, 'rb'))
+            await bot.send_document(BotConfig.CHAT_ID, open(name, 'rb'), caption=message.text)
+        except FileNotFoundError:
+            await bot.send_message(message.chat.id, name)
